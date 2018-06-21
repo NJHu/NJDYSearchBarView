edc3d0ca14a9ecead61cee19151ce0940c441c2c
胡鹏 <64hp@163.com>
2018-06-21 18:31:03 +0800

    update

--- NJDYSearchBarView/Classes/NJDYSearchBarView.swift
+++ NJDYSearchBarView/Classes/NJDYSearchBarView.swift
@@ -1,15 +1,17 @@
 
 import UIKit
+import NJKit
 
 public class NJDYSearchBarView: UIView {
     
-    override init(frame: CGRect) {
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+//        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     override public func awakeFromNib() {
@@ -20,10 +22,16 @@ public class NJDYSearchBarView: UIView {
     override public func layoutSubviews() {
         super.layoutSubviews()
     }
+    static public func searchBarView() -> NJDYSearchBarView {
+        
+        let view = Bundle(for: NJDYSearchBarView.self).loadNibNamed("NJDYSearchBarView", owner: nil, options: nil)?.last
+        
+        return view as! NJDYSearchBarView;
+    }
 }
 
 extension NJDYSearchBarView {
     private func setupUI() {
-        
+        backgroundColor = UIColor.orange
     }
 }
